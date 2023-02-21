433afa8357c6f83cfd26bb747c68049eb242dd89
Magno batista <magnobatista75@gmail.com>
2023-02-21 03:41:00 -0300

    estrutura projeto

--- angular-firebase-jogo-perguntas/src/app/app.module.ts
+++ angular-firebase-jogo-perguntas/src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { PokemonsComponent } from './components/pokemons/pokemons.component';
+import { CardPokemonComponent } from './components/pokemons/card-pokemon/card-pokemon.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonsComponent,
+    CardPokemonComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
